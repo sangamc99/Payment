d6b522f48c3bbbba42d56aba50c5d752784fe5b8
Sangam Chattopadhyay <sangam_chattopadhyay@persistent.com>
2022-04-20 19:50:35 +0530

    Create Order Service

--- src/main/java/com/example/payment/order/OrderService.java
+++ src/main/java/com/example/payment/order/OrderService.java
@@ -3,7 +3,12 @@ package com.example.payment.order;
 import com.example.payment.order.dto.OrderRequest;
 import com.example.payment.order.model.Order;
 import com.example.payment.order.model.OrderStatus;
+import com.razorpay.RazorpayClient;
+import com.razorpay.RazorpayException;
+
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -17,18 +22,43 @@ public class OrderService {
     public OrderService(OrderRepository repository) {
         this.orderRepository = repository;
     }
+    
+    @Value(value = "${razorpay.key}")
+    private String key;
+    @Value(value = "${razorpay.secret}")
+    private String secret;
 
     ResponseEntity<?> createOrder(OrderRequest request) {
         // TODO : create order from Razorpay check for success or failure , set status accordingly and return httpresp
+    	
         Order order = Order.builder()
                 .name(request.getName())
                 .price(Double.parseDouble(request.getPrice()))
                 .status(OrderStatus.PROCESSING)
                 .build();
-
         orderRepository.save(order);
-        return ResponseEntity.status(HttpStatus.OK).body("Order Successfully Created");
+        
+        try {
+        	
+        	RazorpayClient razorpayClient = new RazorpayClient(key, secret);
+        	JSONObject options = new JSONObject();
+        	options.put("amount", Double.parseDouble(request.getPrice()));
+        	options.put("currency", "INR");
+        	options.put("receipt", "txn_123456");
+        	com.razorpay.Order order1 = razorpayClient.Orders.create(options);
+        	
+        	String orderId = order1.get("id");
+        	order.setOrderId(orderId);
+        	order.setStatus(OrderStatus.SUCCESS);
+        	orderRepository.save(order);
+        	
+        } catch(RazorpayException e) {        	
+        	order.setStatus(OrderStatus.FAILED);
+        	orderRepository.save(order);
+        	return ResponseEntity.status(HttpStatus.BAD_GATEWAY).body("Order Not Created");
+        	
+        }  
+        
+        return ResponseEntity.status(HttpStatus.OK).body(order);
     }
-
-
-}
+}	
